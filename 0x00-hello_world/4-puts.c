90cb220ae9b93b0eb50779fdc703a5c34b6be3a4
habiba <habiiba0099@gmail.com>
2023-08-10 16:55:41 -0700

    puts1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #!/bin/bash
 #include <stdio.h>
 
-int main (void)
+int main ()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

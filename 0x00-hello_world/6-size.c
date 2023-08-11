95d6a018e9ae22aff461e8845bf69ff9fdec338e
habiba <habiiba0099@gmail.com>
2023-08-10 17:46:01 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ * Description: a program that prints out the size of various types
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("Size of a char:", sizeof(char);
+	printf("Size of an int:", sizeof(int);
+	printf("Size of a long int:", sizeof(long int);
+	printf("Size of a long long int:", sizeof(long long int);
+	printf("Size of a float:", sizeof(float);
+	
+	return (0);
+
+}

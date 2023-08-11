fdc9f80092952e23ce631ab20efdcdd53f975a75
habiba <habiiba0099@gmail.com>
2023-08-10 17:24:05 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ * Description: a function that prints out some text using printf
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}

ee1b362bc568c6e72b940156b22afd0be5f60b43
habiba <habiiba0099@gmail.com>
2023-08-11 17:30:55 -0700

    alphapet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ *
+ * Description: alphapet
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+}

375420cff0769fdec161b277de60c5bccdb4a1ae
habiba <habiiba0099@gmail.com>
2023-08-11 18:43:27 -0700

    reverse alphapet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - an entry point
+ *
+ *Description:  prints reversed alphapet
+ *
+ *Return: returns 0
+*/
+int main(void)
+{
+	char c;
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n')
+
+	return (0);
+}

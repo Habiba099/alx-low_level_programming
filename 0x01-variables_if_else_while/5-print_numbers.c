8d543160cd0a925bb9e2455c8f4257fb625ce357
habiba <habiiba0099@gmail.com>
2023-08-11 18:07:47 -0700

    numpers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - an entry point
+ *
+ *Description: prints numbers
+ *
+ *Return: returns 0
+*/
+
+int main(void)
+{
+	int x;
+
+	while (x < 10)
+	{
+		printf("%d \n", x);
+		x++;
+	}
+
+	return (0);
+}

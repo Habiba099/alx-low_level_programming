39d62b33bd52c7febae02451a91121971dc4b0d6
habiba <habiiba0099@gmail.com>
2023-08-11 19:47:18 -0700

    digits0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - an entry point
+ *
+ *Description:a program that prints all possible combinations of numbers
+ *
+ *Return: returns 0
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}

bac7796bf2447e2f922c5a9eeda38e161d535312
habiba <habiiba0099@gmail.com>
2023-08-11 19:37:57 -0700

    hexa6

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,26 +5,25 @@
  * Description: a program that prints all the numbers of base 16 in lowercase
  *
  * Return: returns 0
- **/
-
+ */
 int main(void)
 {
-        char x = '0';
-        char c = 'a';
+	char x = '0';
+	char c = 'a';
 
-        while (x <= '9')
-        {
-                putchar(x);
-                x++;
-        }
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
 
-        while (c <= 'f')
-        {
-                /*if ((x >= '0' && x <= '9') || (x >= 'a' && x <= 'f'))*/
-                putchar(c);
-                c++;
-        }
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
 
-        putchar('\n');
+	putchat('\n');
 
-        return (0);}
+	return (0);
+}

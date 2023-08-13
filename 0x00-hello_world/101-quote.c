b2d8511814594e7cc8343d386dce86a786f7d702
habiba <habiiba0099@gmail.com>
2023-08-13 06:12:24 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Unix
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
+
+	putchar(str[i]);
+
+	return (0);
+}

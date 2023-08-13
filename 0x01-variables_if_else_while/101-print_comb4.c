80b4f65e10c694ecb884b189b4666909ed486e2d
habiba <habiiba0099@gmail.com>
2023-08-13 02:48:19 -0700

    d

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ *
+ * Description: print numbers
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int x, i, z;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		i = x + 1;
+
+		while (i < 10)
+		{
+			z = i + 1;
+			putchar(x + 48);
+			putchar(i + 48);
+			putchar(z + 48);
+			if (x != 7 || i != 8 || z != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+

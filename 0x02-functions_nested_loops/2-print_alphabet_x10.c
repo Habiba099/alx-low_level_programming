860fe6cbffc4fe9201b3b6be18a08f6e73d95244
habiba <habiiba0099@gmail.com>
2023-08-15 10:37:45 -0700

    alphabet_part2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alpha 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+	}
+}

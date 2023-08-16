e6ab508bfc68941358f0431e0b598d1d97c9506e
habiba <habiiba0099@gmail.com>
2023-08-15 19:02:57 -0700

    last digit0

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  prints the last digit of a number
+ *
+ * @i: integer
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int i)
+{
+	int ldigit = i % 10;
+
+	return (ldigit);
+
+}

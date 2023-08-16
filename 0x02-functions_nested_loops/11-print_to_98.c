4d2a94cf5d06240d4cc8e4361e70c4d4c58758c2
habiba <habiiba0099@gmail.com>
2023-08-16 10:28:43 -0700

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,11 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	i = n;
-
-
-	while (i <= 98)
+	while (n <= 98)
 	{
-		putchar(i);
+		putchar(n);
 
-		if (i != 98)
+		if (n != 98)
 		{
 			putchar(',');
 			putchar(' ');

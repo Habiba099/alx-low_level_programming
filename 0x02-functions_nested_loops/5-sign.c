cee0c16330257077e414fdfbe56ff4f44f91e562
habiba <habiiba0099@gmail.com>
2023-08-15 18:28:03 -0700

    sign4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@
  */
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		putchar(48);
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		putchar(43);
 		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return (0);
 	}
 	else
 	{

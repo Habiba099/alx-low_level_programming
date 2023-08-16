a73272fbc087281936c89964bfdfe30f667c4bd7
habiba <habiiba0099@gmail.com>
2023-08-15 18:00:28 -0700

    h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign -  prints the sign of a number
+ *
+ * @n: is a number
+ *
+ * Returns: 1 if n is greater than zero
+ * and 0 if n is zero and -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	n = 0;
+
+	if (n)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}

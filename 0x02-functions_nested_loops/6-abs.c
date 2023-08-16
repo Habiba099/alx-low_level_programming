3659253bad91023d774c39316384e0e861da79cd
habiba <habiiba0099@gmail.com>
2023-08-15 18:48:23 -0700

    abs_value0

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of an integer
+ *
+ * @int: integer
+ *
+ * Return: the absolute value
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+
+}

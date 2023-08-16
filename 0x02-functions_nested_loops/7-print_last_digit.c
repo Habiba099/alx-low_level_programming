e75d0ed23d1f45f7dac817093d3e09279aaa9d27
habiba <habiiba0099@gmail.com>
2023-08-15 19:34:46 -0700

    last digit5

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,31 @@
 /**
  * print_last_digit -  prints the last digit of a number
  *
+ * _abs - print the abs
+ *
  * @i: integer
  *
  * Return: Always 0.
  */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+
+}
+
 int print_last_digit(int i)
 {
 	int ldigit = i % 10;
 
-	if (i < 0)
-	{
-		return (-i);
-		_putchar(ldigit + '0');
-	}
+	ldigit = _abs(ldigit);
+
+	_putchar(ldigit + '0');
 	return (ldigit);
 }

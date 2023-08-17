657672e8cc6ac0b4e82b5ab666543ec29357650b
habiba <habiiba0099@gmail.com>
2023-08-17 07:07:56 -0700

    multiply0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two functions
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: return 0
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}

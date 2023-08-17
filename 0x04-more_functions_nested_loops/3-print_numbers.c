f1f34c228db75d6e9f67d38fe468b8d9fa3115e3
habiba <habiiba0099@gmail.com>
2023-08-17 07:14:19 -0700

    from0to9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Description: prints numbers from 0 to 9
+ *
+ * Return: doesn't return a value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i >= 48 && i <= 57)
+	{
+		putchar(i);
+		putchar(' ');
+		i++;
+	}
+}

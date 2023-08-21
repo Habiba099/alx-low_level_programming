bbba8e7f1487375d82bcdbb17cad9a59987be31f
habiba <habiiba0099@gmail.com>
2023-08-21 16:23:34 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _strlen - prints length of str
+ *
+ * @s: char
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (len != 0)
+	{
+		len++;
+		s++;
+	}
+
+	return (0);
+}

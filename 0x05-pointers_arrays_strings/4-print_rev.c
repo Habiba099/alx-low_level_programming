a0e14693f90441ab73d445fdab48b94de638d792
habiba <habiiba0099@gmail.com>
2023-08-24 02:09:11 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: a pointer to string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int length =0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	
+	i = length - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}

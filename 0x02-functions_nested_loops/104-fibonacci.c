e1cdf5ba7b53c46d2e0b68739c69f516e423ca19
habiba <habiiba0099@gmail.com>
2023-08-27 04:34:55 -0700

    fibonacci1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,25 +8,24 @@
  */
 int print_fibonacci()
 {
-        int arr[98];
-        arr[0] = 1;
-        arr[1] = 2;
-        int i;
+	int arr[98];
+	arr[0] = 1;
+	arr[1] = 2;
 
-        printf("%d, ", arr[0]);
-        printf("%d, ", arr[1]);
+	printf("%d, ", arr[0]);
+	printf("%d, ", arr[1]);
 
-        for (i = 2; i < 98; i++)
-        {
-                arr[i] = arr[i-1] + arr[i-2];
-                printf("%d, ", arr[i]);
-        }
+	for (int i = 2; i < 98; i++)
+	{
+		arr[i] = arr[i-1] + arr[i-2];
+		printf("%d, ", arr[i]);
+	}
 }
 
 int main()
 {
 
-    print_fibonacci();
+	print_fibonacci();
 
-    return 0;
+	return (0);
 }

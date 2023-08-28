36014889fc12c9e75f3f49eed5b9b7b277b165f8
habiba <habiiba0099@gmail.com>
2023-08-28 11:46:36 -0700

    fizz0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * fizz_buzz - prints numbers
+ *
+ * Description: prints numbers
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+	int arr[100];
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		arr[i] = i + 1;
+
+		if (arr[i] % 3 == 0 && arr[i] % 5 == 0)
+		{
+			printf("FizzBuzz, ");
+			continue;
+		}
+
+		if (arr[i] % 3 == 0)
+		{
+			printf("Fizz, ");
+			continue;
+		}
+
+		else if (arr[i] % 5 == 0)
+		{
+			printf("Buzz, ");
+			continue;
+		}
+
+		else
+		{
+			printf("%d", arr[i]);
+		}
+
+		if (i < 99)
+		{
+			printf(", ");
+		}
+	}
+}
+/**
+ * main - entry point
+ *
+ * Description: entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	fizz_buzz();
+
+	return (0);
+}

1144f6077ffacbc4c38f995e4d5be34b83de5d96
habiba <habiiba0099@gmail.com>
2023-09-04 08:33:07 -0700

    dup0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicates strings
+ *
+ * @str: pointer
+ *
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *n = malloc(sizeof(*str) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < len)
+	{
+		n[i] = str[i];
+		i++;
+	}
+	return (n);
+	free(n);
+
+}

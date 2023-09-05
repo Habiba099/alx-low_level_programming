82a2f4df73d8f428cf3cb61e1626c9dac2eff661
habiba <habiiba0099@gmail.com>
2023-09-05 12:49:14 -0700

    dup8

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,8 +12,6 @@ char *_strdup(char *str)
 	int i = 0, len = 0;
 	char *p;
 
-	p = malloc((len + 1) * sizeof(char));
-
 	if (str == NULL)
 	{
 		return (NULL);
@@ -22,6 +20,9 @@ char *_strdup(char *str)
 	{
 		len++;
 	}
+
+	p = malloc((len + 1) * sizeof(char));
+
 	if (p == NULL)
 	{
 		return (NULL);

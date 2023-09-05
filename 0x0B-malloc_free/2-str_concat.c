0dc0a19ac2197b54ab47d648bd1fdd126f536873
habiba <habiiba0099@gmail.com>
2023-09-05 13:05:59 -0700

    concat1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,21 +16,23 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		return (str_concat(NULL, s2));
+	}
+	if (s2 == NULL)
+	{
+		return (str_concat(s1, NULL));
+	}
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	p = malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	while (i < len1)
 	{
 		p[i] = s1[i];

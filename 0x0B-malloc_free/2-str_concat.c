7bfb12224e8e2bf5837ab0e8b7e3b649ef1c81ad
habiba <habiiba0099@gmail.com>
2023-10-30 01:18:46 -0700

    malloc008

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "\0";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "\0";
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		s1 = s2 = "\0";
 	}
 
 	while (s1[len1] != '\0')

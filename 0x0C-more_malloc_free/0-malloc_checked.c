add5148e1e81bf50dcf13293cb88183dc5fe7488
habiba <habiiba0099@gmail.com>
2023-11-24 05:45:17 -0800

    more_malloc0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/*
+ * malloc_checked - allocate memory
+ *
+ * description: returns pointers
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: pointers
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *c;
+	int *i;
+	float *f;
+	double *d;
+
+	c = (char *) malloc(b);
+	i = (int *) malloc(b);
+	f = (float *) malloc(b);
+	d = (double *) malloc(b);
+
+	if (c == NULL)
+		exit(98);
+	if (i == NULL)
+		exit(98);
+	if (f == NULL)
+		exit(98);
+	if (d == NULL)
+		exit(98);
+
+	return (c);
+	return (i);
+	return (f);
+	return (d);
+
+	free(c);
+	free(i);
+	free(f);
+	free(d);
+}

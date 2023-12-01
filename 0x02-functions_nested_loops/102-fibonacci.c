fd02a380cbd67783acddeb58612974a286faad59
habiba <habiiba0099@gmail.com>
2023-12-01 01:08:54 -0800

    fib0

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: prints first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long long arr[50], i;
+	arr[0] = 1;
+	arr[1] = 2;
+
+	for (i = 2; i < 50; i++)
+	{
+
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%llu ", arr[i]);
+	}
+
+	//printf("\n");
+
+	return (0);
+}

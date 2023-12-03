fced81484ab74a648e9c5c3765b2f9d348c8b545
habiba <habiiba0099@gmail.com>
2023-12-03 01:20:45 -0800

    mul4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int result1 = atoi(argv[1]);
-	int result2 = atoi(argv[2]);
+	if (argc == 3)
+	{
+		int result1 = atoi(argv[1]);
+		int result2 = atoi(argv[2]);
 
-	printf("%d\n", result1 * result2);
+		printf("%d\n", result1 * result2);
+	}
 
-	if (argc == 1)
+	else
 	{
 		printf("Error\n");
 	}

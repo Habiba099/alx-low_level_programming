319b711be95d786011b58177adcf1eca5ad117ac
habiba <habiiba0099@gmail.com>
2023-12-03 00:39:19 -0800

    CLI2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *@argc: number of arguments
+ *@argv: pointer array to the arguments
+ *
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

46e0a7ca1c10d96af3205a3910bb592f193fdef8
habiba <habiiba0099@gmail.com>
2023-12-03 01:07:51 -0800

    mul0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ *@argc: number of arguments
+ *@argv: pointer array to the arguments
+ *
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+		int result1 = atoi(argv[1]);
+		int result2 = atoi(argv[2]);
+
+		printf("%d\n", result1 * result2);
+		
+		if (argc == 1)
+		{
+				printf("Error\n");
+		}
+		return (0);
+}
+

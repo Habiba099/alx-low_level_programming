9993d9e0262a4e00e6f4bf19fac5da3b784957a4
habiba <habiiba0099@gmail.com>
2023-12-03 00:19:35 -0800

    CLI0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *@argc: number of arguments
+ *@argv: pointer array to the arguments
+ *
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s \n", argv[0]);
+	}
+
+	return (0);
+}
